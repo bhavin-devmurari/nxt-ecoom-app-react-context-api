642338b9670f6e5db19cfac09066380bfc5a454e
bDev915 <devmuraribhavin90@gmail.com>
2022-04-30 19:27:18 +0530

    Next js application with react context api, mongoD

---
+++ utils/styles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10, //It will by default take it as "10px"
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
+
+export default useStyles;
